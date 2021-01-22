182a03a95fdb99b1913449a151566875b7b7ae55
ChickenDr <fkdlrj1230@gmail.com>
2021-01-22 14:45:12 +0000

    삽입, 삭제, 저장, 불러오기, 채킹, 임시 clear 버튼

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
+// time, clear
 const dateYear = document.getElementById('year');
 const dateMonth = document.getElementById('date');
+const clear = document.getElementById('clear');
 
-// 시간을 로드
+// list, input
+const list = document.getElementById('list');
+const input = document.getElementById('input_todo');
+
+// todo 객체
+let todoList = [];
+// 노드의 수
+let id = 0;
+
+// 리스트 요소의 상태를 표시하는 값들
+const CHECKED = 'checked';
+const UNCHECK = 'uncheck';
+const LINE = 'todoIsDone';
+
+// todo 객체들을 로컬 스토리지에서 불러오기
+let savedTodo = localStorage.getItem("Todo");
+
+// 만약 저장된 객체가 있으면 list에 추가
+if(savedTodo){
+	todoList = JSON.parse(savedTodo);
+	id = todoList.length;
+	todoList.forEach(arry =>{
+		InsertToDo(arry.content, arry.id, arry.done, arry.del);
+	});
+}
+
+// clear 버튼 이벤트
+clear.addEventListener("click", (event) =>{
+	localStorage.clear();
+	location.reload();
+});
+
+// 할일을 넣는 함수
+function InsertToDo(todo, id, done, del) {
+	if (del) {
+		return;
+	}
+
+	const CHECKING = done ? CHECKED : UNCHECK;
+	const TEXT = done ? LINE : "";
+	
+	const toDo = `
+		<ul id="list">
+            <li class="item">
+                <input type="checkbox" id="${id}" ${CHECKING}>
+                <label for="${id}" id="${id}" class="${CHECKING}"></label>
+                <p class="text ${TEXT}">${todo}</p>
+                <input class="delete" id="${id}" type="button" value="X">
+            </li>
+        </ul>`;
+	
+	const position = 'beforeend';
+
+	list.insertAdjacentHTML(position, toDo);
+}
+
+// 시간을 불러오는 함수
 function getTime() {
 	const date = new Date();
 	const year = date.getFullYear(); //   년
@@ -11,39 +69,6 @@ function getTime() {
 	dateYear.innerText = `${year}:${month}:${nowday}`;
 }
 
-let todoList = [],
-	id = 0;
-
-// value
-const list = document.getElementById('list');
-const input = document.getElementById('input_todo');
-
-// 리스트 요소의 상태를 표시하는 값들
-const CHECKED = 'check';
-const UNCHEKCK = 'uncheck';
-const LINE_THROUGH = 'todoIsDone';
-
-// 할일을 넣는 함수
-function InsertToDo(todo, id, done, del) {
-	if (del) {
-		return;
-	}
-
-	const DONE = done ? CHECKED : UNCHEKCK;
-
-	const toDo = `
-		<ul id="list">
-            <li class="item">
-                <input type="checkbox" id="${id}" class=${DONE}/>
-                <label for="${id}" value="done"></label>
-                <p class="text">${todo};</p>
-                <input class="button" type="button" value="X">
-            </li>
-        </ul>`;
-	const position = 'beforeend';
-
-	list.insertAdjacentHTML(position, toDo);
-}
 
 // 엔터 이벤트 리스너
 document.querySelector('#input_todo').addEventListener('keyup', (event) => {
@@ -51,35 +76,54 @@ document.querySelector('#input_todo').addEventListener('keyup', (event) => {
 		const toDo = input.value;
 		if (toDo) {
 			InsertToDo(toDo, id, false, false);
-			todoList.push({
-				name: toDo,
+			todoList.push({  // 요소의 상태값
+				content: toDo,
 				id: id,
 				done: false,
 				del: false,
 			});
 			id++;
+			
+			// todolist가 업데이트 되었으니 저장
+			localStorage.setItem("Todo", JSON.stringify(todoList));
 		}
 		input.value = '';
 	}
 });
 
-function completeTodo(element) {
+// checkbox를 클릭시 채크 이벤트 함수
+function completeTodo(element) {	
+	// checkbox 상태 요소 변경
 	element.classList.toggle(CHECKED);
-	console.log(element.toggle(CHEKCK));
-	element.classList.toggle(UNCHEKCK);
-	element.parentNode.querySelector('.text').classList.toggle(LINE_THROUGH);
+	element.classList.toggle(UNCHECK);
+	
+	// text line throught
+	element.parentNode.querySelector('.text').classList.toggle(LINE);
 
 	todoList[element.id].done = todoList[element.id].done ? false : true;
 }
 
+// delete 버튼 클릭시 삭제 함수
+function deleteTodo(element) {
+	// ul 리스트에서 선택한 노드를 지운다
+	element.parentNode.parentNode.removeChild(element.parentNode);
+	
+	todoList[element.id].del = true;
+}
 
+// 클릭 이벤트 리스너
 list.addEventListener('click', (event) => {
 	const element = event.target;
-	if (element.checked == true) {
+	const elementStat = element.className;
+
+	if (elementStat == "checked" || elementStat == "uncheck") {
 		completeTodo(element);
-	} else if (element.checked = false) {
-		removeToDo();
+	} else if (elementStat == "delete") {
+		deleteTodo(element);
 	}
+	
+	// todolist가 업데이트 되었으니 저장 
+	localStorage.setItem("Todo", JSON.stringify(todoList));
 });
 
 getTime();
